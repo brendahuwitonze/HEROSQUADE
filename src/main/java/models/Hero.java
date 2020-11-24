20020cd478898d65d68fa5909f046c8fa20cb675
brendahuwitonze <uwitonzebrendah@gmail.com>
2020-11-24 08:56:53 +0200

    added files

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,54 +1,44 @@
 package models;
 
+import javax.swing.text.html.HTMLWriter;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Hero {
+
     private String name;
     private int age;
     private String power;
     private String weakness;
     private int id;
-    private static ArrayList<Hero>instances=new ArrayList<> ();
-    public Hero(String name,int age,String power,String weakness){
-        this.name=name;
-        this.age=age;
-        this.power=power;
-        this.weakness=weakness;
+    private static ArrayList<Hero> instances = new ArrayList<>();
+
+    public Hero(String name, Integer age, String power, String weakness) {
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
         instances.add(this);
-        this.id=instances.size ();
+        this.id = instances.size();
     }
 
-    public static ArrayList<Hero> getAllInstances (){
-        return instances;
-    }
-    public static void clearAllHeroes(){
-        instances.clear();
-    }
+    public String getName() {return this.name;}
+    public int getAge() {return this.age;}
+    public String getPower() {return this.power;}
+    public String getWeakness() {return this.weakness;}
+    public static ArrayList<Hero> getAllInstances() {return instances;}
+    public static void clearAllHeroes(){instances.clear();}
+    public int getId(){return id;}
     public static Hero findById(int id) {return instances.get(id-1);}
 
-    public int getId (){
-        return id;
+    public static Hero setUpNewHero(){
+        return new Hero("Brendah",20,"flying","water");
     }
-
-    public String getWeakness (){
-        return weakness;
+    public static Hero setUpNewHero1(){
+        return new Hero("brian",10,"water","flying");
     }
-
-
-    public String getPower (){
-        return power;
-    }
-
-
-    public int getAge (){
-        return age;
-    }
-
-    public String getName (){
-        return this.name;
-    }
-    public static Hero setNewHero(){
-        return new Hero("Jesus",20,"flying","swimming");
+    public static Hero setUpNewHero2(){
+        return new Hero("joshua",20,"Night Vision","Light");
     }
 
 }
